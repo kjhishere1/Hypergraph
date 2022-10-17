cb9cab40552806eaa8a97a48d742d11e0d69bcd5
kjhishere1 <kjhishere@proton.me>
2022-10-17 18:07:10 +0900

    simplify

--- run.py
+++ run.py
@@ -1,34 +1,12 @@
 import os
 import turtle
 
-import pip
-from importlib import util
-found = True if util.find_spec('rich') is not None else False
-if not found:
-    pip.main(['install', "rich"])
-    os.system('cls')
-
-from rich import box
-from rich.progress import track
-from rich.console import Console
-from rich.table import Table
-
+import util
 from hypergraph import Hypergraph as HG
 
-logo = r"""
- /$$   /$$                                                                                /$$      
-| $$  | $$                                                                               | $$      
-| $$  | $$ /$$   /$$  /$$$$$$   /$$$$$$   /$$$$$$   /$$$$$$   /$$$$$$  /$$$$$$   /$$$$$$ | $$$$$$$ 
-| $$$$$$$$| $$  | $$ /$$__  $$ /$$__  $$ /$$__  $$ /$$__  $$ /$$__  $$|____  $$ /$$__  $$| $$__  $$
-| $$__  $$| $$  | $$| $$  \ $$| $$$$$$$$| $$  \__/| $$  \ $$| $$  \__/ /$$$$$$$| $$  \ $$| $$  \ $$
-| $$  | $$| $$  | $$| $$  | $$| $$_____/| $$      | $$  | $$| $$      /$$__  $$| $$  | $$| $$  | $$
-| $$  | $$|  $$$$$$$| $$$$$$$/|  $$$$$$$| $$      |  $$$$$$$| $$     |  $$$$$$$| $$$$$$$/| $$  | $$
-|__/  |__/ \____  $$| $$____/  \_______/|__/       \____  $$|__/      \_______/| $$____/ |__/  |__/
-           /$$  | $$| $$                           /$$  \ $$                   | $$                
-          |  $$$$$$/| $$                          |  $$$$$$/                   | $$                
-           \______/ |__/                           \______/                    |__/                
+util.pip_install('rich')
+from rich.console import Console
 
-"""
 
 width, height = 512, 512
 Origin = True
@@ -38,22 +16,16 @@ Xcon = 80
 Ycon = 80
 Step = 3
 
-console = Console()
-
-table = Table(show_header=True, header_style="bold")
-table.box = box.SIMPLE
-col = ["평면 크기", "원점", "X축", "Y축", "X축 눈금", "Y축 눈금", "X값 증가량"]
-for text in col:
-	table.add_column(text, justify="center", width=12)
-table.add_row(
-	f"{width}x{height}",
-	"✅" if Origin else "❌",
-	"✅" if Xaxis else "❌",
-	"✅" if Yaxis else "❌",
-	str(Xcon),
-	str(Ycon),
-	str(Step)
+print(util.logo)
+c = Console()
+c.print(
+	util.makeTable(
+		["평면 크기", "원점", "X축", "Y축", "X축 눈금", "Y축 눈금", "X값 증가량"],
+		[f"{width}x{height}", "✅" if Origin else "❌", "✅" if Xaxis else "❌", "✅" if Yaxis else "❌", str(Xcon), str(Ycon), str(Step)]
+	)
 )
+print('Shift + D를 눌러서 나가기')
+
 
 def draw(func, step):
 	hg.step = step
@@ -61,10 +33,6 @@ def draw(func, step):
 	gr = iter(hg._drawFunc())
 	for _ in track(hg.xList):
 		next(gr)
-
-print(logo.rstrip())
-console.print(table)
-print('Shift + D를 눌러서 나가기')
 
 hg = HG(width, height, None, Step)
 if Origin:
---
+++ util.py
@@ -0,0 +1,43 @@
+import os
+import pip
+from importlib.util import find_spec
+
+from rich import box
+from rich.progress import track
+from rich.table import Table
+
+logo = r"""
+ /$$   /$$                                                                                /$$      
+| $$  | $$                                                                               | $$      
+| $$  | $$ /$$   /$$  /$$$$$$   /$$$$$$   /$$$$$$   /$$$$$$   /$$$$$$  /$$$$$$   /$$$$$$ | $$$$$$$ 
+| $$$$$$$$| $$  | $$ /$$__  $$ /$$__  $$ /$$__  $$ /$$__  $$ /$$__  $$|____  $$ /$$__  $$| $$__  $$
+| $$__  $$| $$  | $$| $$  \ $$| $$$$$$$$| $$  \__/| $$  \ $$| $$  \__/ /$$$$$$$| $$  \ $$| $$  \ $$
+| $$  | $$| $$  | $$| $$  | $$| $$_____/| $$      | $$  | $$| $$      /$$__  $$| $$  | $$| $$  | $$
+| $$  | $$|  $$$$$$$| $$$$$$$/|  $$$$$$$| $$      |  $$$$$$$| $$     |  $$$$$$$| $$$$$$$/| $$  | $$
+|__/  |__/ \____  $$| $$____/  \_______/|__/       \____  $$|__/      \_______/| $$____/ |__/  |__/
+           /$$  | $$| $$                           /$$  \ $$                   | $$                
+          |  $$$$$$/| $$                          |  $$$$$$/                   | $$                
+           \______/ |__/                           \______/                    |__/                
+
+""".rstrip()
+
+def pip_install(modules):
+    ins = []
+    if type(modules) == str:
+        if not find_spec(modules):
+            ins.append(modules)
+    elif type(modules) == list:
+        for module in modules:
+            if not find_spec(module):
+                ins.append(module)
+    if ins != []:
+        pip.main(['install', *ins])
+        os.system('cls')
+
+def makeTable(col: list, row: list):
+    table = Table(show_header=True, header_style="bold")
+    table.box = box.SIMPLE
+    for text in col:
+        table.add_column(text, justify="center", width=12)
+    table.add_row(*row)
+    return Table
