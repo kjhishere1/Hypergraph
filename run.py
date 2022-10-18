b04a1e6a4155fe2f8ab8f388d2b4112270de4327
kjhishere1 <kjhishere@proton.me>
2022-10-18 21:56:10 +0900

    설정기능 추가하고 함수 다 util로 몰아넣는 괴상한 짓 시도.

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import os
+import json
 import turtle
 
 import util
@@ -7,13 +8,16 @@ from rich.console import Console
 from rich.progress import track
 from hypergraph import Hypergraph as HG
 
-width, height = 512, 512
-Origin = True
-Xaxis = True
-Yaxis = True
-Xcon = 80
-Ycon = 80
-Step = 3
+while True:
+	try:
+		cfg_check = util.CfgChecker('hypergraph')
+		if cfg_check:
+			Recfg = input("설정을 초기화 하고 재설정 하시겠습니까? (Y/N)\n>: ")
+			if Recfg == 'Y':
+				raise
+	except Exception:
+		util.GenCfg()
+
 
 c = Console()
 c.print(util.logo_c, justify="center")
@@ -26,13 +30,6 @@ c.print(
 c.print('Ctrl + D를 눌러서 나가기', style="bold red")
 
 
-def draw(hg, func, step):
-	hg.step = step
-	hg.function = func
-	gr = iter(hg._drawFunc())
-	for _ in track(hg.xList):
-		next(gr)
-
 hg = HG(width, height, None, Step)
 if Origin:
 	hg.originMark()
@@ -43,31 +40,22 @@ if Yaxis:
 hg.contourX(Xcon)
 hg.contourY(Ycon)
 
-env = {}
-env["locals"]   = None
-env["globals"]  = None
-env["__name__"] = None
-env["__file__"] = None
-env["__builtins__"] = None
-
-errorPrint = lambda msg, e: c.print(f'{msg} [red][b]{e}[/b][/red]')
-
 while True:
 	cere = input(">: f(x)=")
 	if cere == '':
 		exit()
 	try:
-		func = eval("lambda x: " + cere, env)
-		draw(hg, func, Step)
+		func = eval("lambda x: " + cere, util.env)
+		util.draw(hg, func, Step)
 	except SyntaxError as e:
-		errorPrint('수식 문법 오류가 있습니다.', e)
+		util.errorPrint('수식 문법 오류가 있습니다.', e)
 	except NameError as e:
-		errorPrint('정의 되지 않은 변수를 사용 했습니다.', e)
+		util.errorPrint('정의 되지 않은 변수를 사용 했습니다.', e)
 	except TypeError as e:
-		errorPrint('수식 문법 오류가 있습니다.', e)
+		util.errorPrint('수식 문법 오류가 있습니다.', e)
 	except ZeroDivisionError as e:
-		errorPrint('분모가 0이 될 수 없습니다.', e)
+		util.errorPrint('분모가 0이 될 수 없습니다.', e)
 	except OverflowError as e:
-		errorPrint('숫자 값이 너무 큽니다.', e)
+		util.errorPrint('숫자 값이 너무 큽니다.', e)
 	except Exception as e:
-		errorPrint('알 수 없는 오류가 발생했습니다.', e)
+		util.errorPrint('알 수 없는 오류가 발생했습니다.', e)
--- util.py
+++ util.py
@@ -1,37 +1,10 @@
 import os
 import pip
+import ast
 import json
+
 from importlib.util import find_spec
 
-logo = r"""
- /$$   /$$                                                                                /$$      
-| $$  | $$                                                                               | $$      
-| $$  | $$ /$$   /$$  /$$$$$$   /$$$$$$   /$$$$$$   /$$$$$$   /$$$$$$  /$$$$$$   /$$$$$$ | $$$$$$$ 
-| $$$$$$$$| $$  | $$ /$$__  $$ /$$__  $$ /$$__  $$ /$$__  $$ /$$__  $$|____  $$ /$$__  $$| $$__  $$
-| $$__  $$| $$  | $$| $$  \ $$| $$$$$$$$| $$  \__/| $$  \ $$| $$  \__/ /$$$$$$$| $$  \ $$| $$  \ $$
-| $$  | $$| $$  | $$| $$  | $$| $$_____/| $$      | $$  | $$| $$      /$$__  $$| $$  | $$| $$  | $$
-| $$  | $$|  $$$$$$$| $$$$$$$/|  $$$$$$$| $$      |  $$$$$$$| $$     |  $$$$$$$| $$$$$$$/| $$  | $$
-|__/  |__/ \____  $$| $$____/  \_______/|__/       \____  $$|__/      \_______/| $$____/ |__/  |__/
-           /$$  | $$| $$                           /$$  \ $$                   | $$                
-          |  $$$$$$/| $$                          |  $$$$$$/                   | $$                
-           \______/ |__/                           \______/                    |__/                
-
-""".rstrip()
-
-logo_c = r"""
- /$$   /$$                                                                                /$$      
-| $$  | $$                                                                               | $$      
-     | $$  | $$ /$$   /$$  /$$$$$$   /$$$$$$   /$$$$$$   /$$$$$$   /$$$$$$  /$$$$$$   /$$$$$$ | $$$$$$$ 
-     | $$$$$$$$| $$  | $$ /$$__  $$ /$$__  $$ /$$__  $$ /$$__  $$ /$$__  $$|____  $$ /$$__  $$| $$__  $$
-     | $$__  $$| $$  | $$| $$  \ $$| $$$$$$$$| $$  \__/| $$  \ $$| $$  \__/ /$$$$$$$| $$  \ $$| $$  \ $$
-     | $$  | $$| $$  | $$| $$  | $$| $$_____/| $$      | $$  | $$| $$      /$$__  $$| $$  | $$| $$  | $$
-     | $$  | $$|  $$$$$$$| $$$$$$$/|  $$$$$$$| $$      |  $$$$$$$| $$     |  $$$$$$$| $$$$$$$/| $$  | $$
-     |__/  |__/ \____  $$| $$____/  \_______/|__/       \____  $$|__/      \_______/| $$____/ |__/  |__/
-/$$  | $$| $$                           /$$  \ $$                   | $$                
-|  $$$$$$/| $$                          |  $$$$$$/                   | $$                
-\______/ |__/                           \______/                    |__/                
-
-""".rstrip()
 
 def pip_install(modules):
     ins = []
@@ -46,9 +19,11 @@ def pip_install(modules):
         pip.main(['install', *ins])
         os.system('cls')
 
+
 pip_install('rich')
 from rich import box
 from rich.table import Table
+
 
 def makeTable(col: list, row: list):
     table = Table(show_header=True, header_style="bold")
@@ -58,14 +33,24 @@ def makeTable(col: list, row: list):
     table.add_row(*row)
     return table
 
+
+def draw(hg, func, step):
+	hg.step = step
+	hg.function = func
+	gr = iter(hg._drawFunc())
+	for _ in track(hg.xList):
+		next(gr)
+
+
 def TypeCheck(value: str, type_type):
-    typed = ast.literal_eval(value)
+    typed = ast.literal_eval(str(value))
     return type(typed) == type_type
+
 
 def GetCfgPath(cfg_path):
     PATH = os.path.join(os.getenv('APPDATA'), cfg_path)
     CFG = os.path.join(PATH, r"config.json")
-    if os.path.exists(PATH):
+    if os.path.exists(CFG):
         return CFG
     else:
         os.mkdir(PATH)
@@ -73,9 +58,77 @@ def GetCfgPath(cfg_path):
             file.write('{}')
         return CFG
 
-def CfgChecker():
-    with open(GetCfgPath('hypergraph'), 'r', encoding="UTF-8") as file:
+
+def CfgChecker(cfg_path):
+    with open(GetCfgPath(cfg_path), 'r', encoding="UTF-8") as file:
         cfg = file.read()
         cfg = json.loads(cfg)
-    for key, value in enumerate(cfg):
-        ...
+
+    for key in cfg.keys():
+        value = cfg[key]
+        if len(cfg.keys()) == 7:
+            raise ValueError("설정 파일의 값에 문제가 생겼습니다.")
+
+        if key == "size":
+            assert TypeCheck(value['width'], int) is True, "width값이 정수가 아닙니다."
+            assert TypeCheck(value['height'], int) is True, "height값이 정수가 아닙니다."
+        elif key == "origin":
+            assert TypeCheck(value, bool) is True, "origin값이 Bool이 아닙니다."
+        elif key == "xaxis":
+            assert TypeCheck(value, bool) is True, "xaxis값이 Bool이 아닙니다."
+        elif key == "yaxis":
+            assert TypeCheck(value, bool) is True, "yaxis값이 Bool이 아닙니다."
+        elif key == "xcon":
+            assert TypeCheck(value, int) is True, "xcon값이 정수가 아닙니다."
+        elif key == "ycon":
+            assert TypeCheck(value, int) is True, "ycon값이 정수가 아닙니다."
+        elif key == "step":
+            assert TypeCheck(value, int) is True, "step값이 정수가 아닙니다."
+        else:
+            raise TypeError("정의되지 않은 값입니다.")
+    return True
+
+
+def GenCfg(cfg_path):
+    if (width := input('좌표평면의 가로 크기를 적어주세요.\n(기본값: 512) >: ')) == '':
+        width = 512
+    if (height := input('좌표평면의 세로 크기를 적어주세요.\n(기본값: 512) >: ')) == '':
+        height = 512
+    if (origin := input('좌표평면에서 원점을 표시 할까요? (Y/N)\n(기본값: Y) >: ')) == '':
+        origin = True
+    if (xaxis := input('좌표평면에서 X축을 표시 할까요? (Y/N)\n(기본값: Y) >: ')) == '':
+        xaxis = True
+    if (yaxis := input('좌표평면에서 Y축을 표시 할까요? (Y/N)\n(기본값: Y) >: ')) == '':
+        yaxis = True
+    if (xcon := input('좌표평면에서 X축 눈금을 표시 할까요? (Y/N)\n(기본값: Y) >: ')) == '':
+        xcon = True
+    if (ycon := input('좌표평면에서 Y축 눈금을 표시 할까요? (Y/N)\n(기본값: Y) >: ')) == '':
+        ycon = True
+    if (step := input('X의 정의역을 몇의 배수로 설정하시겠습니까?\n(기본값: 3) >: ')) == '':
+        step = 3
+
+    cfg = {}
+    cfg["size"] = {"width": width, "height": height}
+    cfg["origin"] = origin
+    cfg["xaxis"] = xaxis
+    cfg["yaxis"] = yaxis
+    cfg["xcon"] = xcon
+    cfg["ycon"] = ycon
+    cfg["step"] = step
+
+    cfg = json.joins(cfg)
+
+    with open(GetCfgPath(cfg_path, 'w', encoding="UTF-8")) as file:
+        file.write(cfg)
+
+logo = f"\n /$$   /$${' '*80}/$$\n| $$  | $${' '*79}| $$\n| $$  | $$ /$$   /$$  /$$$$$$   /$$$$$$   /$$$$$$   /$$$$$$   /$$$$$$  /$$$$$$   /$$$$$$ | $$$$$$$\n| $$$$$$$$| $$  | $$ /$$__  $$ /$$__  $$ /$$__  $$ /$$__  $$ /$$__  $$|____  $$ /$$__  $$| $$__  $$\n| $$__  $$| $$  | $$| $$  \ $$| $$$$$$$$| $$  \__/| $$  \ $$| $$  \__/ /$$$$$$$| $$  \ $$| $$  \ $$\n| $$  | $$| $$  | $$| $$  | $$| $$_____/| $$      | $$  | $$| $$      /$$__  $$| $$  | $$| $$  | $$\n| $$  | $$|  $$$$$$$| $$$$$$$/|  $$$$$$$| $$      |  $$$$$$$| $$     |  $$$$$$$| $$$$$$$/| $$  | $$\n|__/  |__/ \____  $$| $$____/  \_______/|__/       \____  $$|__/      \_______/| $$____/ |__/  |__/\n{' '*11}/$$  | $$| $${' '*27}/$$  \ $${' '*19}| $$\n{' '*10}|  $$$$$$/| $${' '*26}|  $$$$$$/{' '*19}| $$\n{' '*11}\______/ |__/{' '*27}\______/{' '*20}|__/"
+logo_c = '\n'.join([*logo.split('\n')[:3], *[" "*5 + l for l in logo.split('\n')[3:9]], *[i.lstrip() for i in logo.split('\n')[9:]]])
+
+env = {
+    "locals": None,
+    "globals": None,
+    "__name__": None,
+    "__file__": None,
+    "__builtins__": None
+}
+errorPrint = lambda msg, e: c.print(f'{msg} [red][b]{e}[/b][/red]')
