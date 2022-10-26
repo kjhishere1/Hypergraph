318d5777c3c3700d9956ffe6dbb6dbe4d5983f16
kjhishere1 <kjhishere@proton.me>
2022-10-26 11:06:16 +0900

    더 나은 구조

--- hypergraph.py
+++ hypergraph.py
@@ -1,131 +1,146 @@
+import re
+
 import turtle
 from turtle import Turtle, Screen
 
+
 class Hypergraph:
-	def __init__(self, width, height, function, step):
-		self.function = function
-		self.step = step
+    def __init__(self, width, height, function, step):
+        self.function = function
+        self.step = step
 
-		self.width = width
-		self.height = height
+        self.width = width
+        self.height = height
 
-		self.xMax = width/2
-		self.xMin = (width/2)*-1
-		self.yMax = height/2
-		self.yMin = (height/2)*-1
+        self.xMax = width/2
+        self.xMin = (width/2)*-1
+        self.yMax = height/2
+        self.yMin = (height/2)*-1
 
-		self.xList = Hypergraph._BetterRange(int(self.xMax), int(self.xMin), self.step)
+        self.xList = Hypergraph._BetterRange(
+            int(self.xMax), int(self.xMin), self.step)
 
-		self.screen = self._screenSetup()
-		self.turtle = self._turtleSetup()
+        self.screen = self._screenSetup()
+        self.turtle = self._turtleSetup()
 
-	def _screenSetup(self):
-		screen = Screen()
-		screen.setup(self.width, self.height)
-		return screen
+    def _screenSetup(self):
+        screen = Screen()
+        screen.setup(self.width, self.height)
+        return screen
 
-	def _turtleSetup(self):
-		tu = Turtle()
-		tu.hideturtle()
-		tu.speed(0)
-		return tu
+    def _turtleSetup(self):
+        tu = Turtle()
+        tu.hideturtle()
+        tu.speed(0)
+        return tu
 
-	def _move(self, x, y):
-		self.turtle.penup()
-		self.turtle.goto(x, y)
-		self.turtle.pendown()
+    def _move(self, x, y):
+        self.turtle.penup()
+        self.turtle.goto(x, y)
+        self.turtle.pendown()
 
-	def originMark(self):
-		self._move(-10, -15)
-		self.turtle.write("O")
+    def originMark(self):
+        self._move(-10, -15)
+        self.turtle.write("O")
 
-	def drawX(self):
-		self._move(self.xMax, 0)
-		self.turtle.goto(self.xMin, 0)
+    def drawX(self):
+        self._move(self.xMax, 0)
+        self.turtle.goto(self.xMin, 0)
 
-	def drawY(self):
-		self._move(0, self.yMax)
-		self.turtle.goto(0, self.yMin)
+    def drawY(self):
+        self._move(0, self.yMax)
+        self.turtle.goto(0, self.yMin)
 
-	def contourX(self, grad):
-		if grad <= 0:
-			return None
+    def contourX(self, grad):
+        if grad <= 0:
+            return None
 
-		self._move(0, 0)
+        self._move(0, 0)
 
-		goX = 0
-		while (self.xMin < goX):
-			goX += (grad)*-1
-			self._move(goX, 5)
-			self.turtle.goto(goX, -5)
+        goX = 0
+        while (self.xMin < goX):
+            goX += (grad)*-1
+            self._move(goX, 5)
+            self.turtle.goto(goX, -5)
 
-			self._move(goX-5, -20)
-			self.turtle.write(goX)
+            self._move(goX-5, -20)
+            self.turtle.write(goX)
 
-		backX = 0
-		while (self.xMax > backX):
-			backX += grad
-			self._move(backX, 5)
-			self.turtle.goto(backX, -5)
+        backX = 0
+        while (self.xMax > backX):
+            backX += grad
+            self._move(backX, 5)
+            self.turtle.goto(backX, -5)
 
-			self._move(backX-8, -20)
-			self.turtle.write(backX)
+            self._move(backX-8, -20)
+            self.turtle.write(backX)
 
-	def contourY(self, grad):
-		if grad <= 0:
-			return None
-		self._move(0, 0)
+    def contourY(self, grad):
+        if grad <= 0:
+            return None
+        self._move(0, 0)
 
-		goY = 0
-		while (self.yMin < goY):
-			goY += (grad)*-1
-			self._move(5, goY)
-			self.turtle.goto(-5, goY)
+        goY = 0
+        while (self.yMin < goY):
+            goY += (grad)*-1
+            self._move(5, goY)
+            self.turtle.goto(-5, goY)
 
-			self._move(10, goY-8)
-			self.turtle.write(goY)
+            self._move(10, goY-8)
+            self.turtle.write(goY)
 
-		backY = 0
-		while (self.yMax > backY):
-			backY += grad
-			self._move(5, backY)
-			self.turtle.goto(-5, backY)
+        backY = 0
+        while (self.yMax > backY):
+            backY += grad
+            self._move(5, backY)
+            self.turtle.goto(-5, backY)
 
-			self._move(10, backY-8)
-			self.turtle.write(backY)
+            self._move(10, backY-8)
+            self.turtle.write(backY)
 
-	@staticmethod
-	def _BetterRange(start, stop, step=1):
-		rangeList = []
-		rangeList = [*rangeList, *reversed(range(0, start, step))]
-		rangeList = [*rangeList, *[i*-1 for i in list(range(0, stop*-1, step))]]
-		return list(reversed(sorted(list(set(rangeList)))))
+    @staticmethod
+    def _BetterRange(start, stop, step=1):
+        rangeList = []
+        rangeList = [*rangeList, *reversed(range(0, start, step))]
+        rangeList = [*rangeList, *
+                     [i*-1 for i in list(range(0, stop*-1, step))]]
+        return list(reversed(sorted(list(set(rangeList)))))
 
-	def _drawFunc(self):
-		self._move(self.xMax, self.function(int(self.xMax)))
-		for x in self.xList:
-			try:
-				y = self.function(x)
-			except ZeroDivisionError as e:
-				x = self.xList[self.xList.index(x) + 1]
-				y = self.function(x)
-				self._move(x, y)
-			else:
-				self.turtle.goto(x, y)
-			yield x, y
+    @staticmethod
+    def Parse(string: str):
+        string = string.lower()
+        string = string.replace("^", "**")
+        roots = re.findall(r"root\((.+?)\)", string)
+        for rt in roots:
+            string = string.replace(f'root({rt})', f"(({rt})**(1/2))")
+        return string
 
-	def Graph(self):
-		for x, y in self._drawFunc():
-			pass
+    def _drawFunc(self, track=lambda x: x):
+        self._move(self.xMax, self.function(int(self.xMax)))
+        for x in track(self.xList):
+            try:
+                y = self.function(x)
+            except ZeroDivisionError as e:
+                x = self.xList[self.xList.index(x) + 1]
+                y = self.function(x)
+                self._move(x, y)
+            else:
+                self.turtle.goto(x, y)
+            yield x, y
+
+    def Graph(self):
+        for x, y in self._drawFunc():
+            pass
+
 
 if __name__ == '__main__':
-	func = lambda x: -1*(((1/10)*x)**2)
-	hg = Hypergraph(512, 512, func, 10)
-	hg.originMark()
-	hg.drawX()
-	hg.drawY()
-	hg.contourX(30)
-	hg.contourY(30)
-	hg.Graph()
+    def func(x): return -1*(((1/10)*x)**2)
+    hg = Hypergraph(512, 512, func, 10)
+    hg.originMark()
+    hg.drawX()
+    hg.drawY()
+    hg.contourX(30)
+    hg.contourY(30)
+    hg.Graph()
 
-	turtle.exitonclick()
+    turtle.exitonclick()
--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 import os
 import json
 import turtle
+import random
 
 import util
 
@@ -9,85 +10,80 @@ from rich.progress import track
 from hypergraph import Hypergraph as HG
 
 while True:
-	try:
-		cfg_check = util.CfgChecker('hypergraph')
-		if cfg_check:
-			Recfg = input("설정을 초기화 하고 재설정 하시겠습니까? (Y를 눌러 확인)\n>: ")
-			if Recfg == 'Y':
-				raise
-			else:
-				break
-	except Exception:
-		util.GenCfg('hypergraph')
+    try:
+        cfg_check = util.CfgChecker('hypergraph')
+        if cfg_check:
+            Recfg = input("설정을 초기화 하고 재설정 하시겠습니까? (Y를 눌러 확인)\n>: ")
+            if Recfg == 'Y':
+                raise
+            else:
+                break
+    except Exception:
+        util.GenCfg('hypergraph')
 
 with open(util.GetCfgPath('hypergraph'), 'r', encoding="UTF-8") as file:
-	cfg = json.loads(file.read())
+    cfg = json.loads(file.read())
 
 
 c = Console()
 c.print(util.logo_c, justify="center")
 c.print(
-	util.makeTable(
-		["평면 크기", "원점", "X축", "Y축", "X축 눈금", "Y축 눈금", "X값 증가량"],
-		[
-			f"{cfg['size']['width']}x{cfg['size']['height']}",
-			"✅" if cfg['origin'] else "❌",
-			"✅" if cfg['xaxis'] else "❌",
-			"✅" if cfg['yaxis'] else "❌",
-			str(cfg['xcon']),
-			str(cfg['ycon']),
-			str(cfg['step'])
-		]
-	)
+    util.makeTable(
+        ["평면 크기", "원점", "X축", "Y축", "X축 눈금", "Y축 눈금", "X값 증가량"],
+        [
+            f"{cfg['size']['width']}x{cfg['size']['height']}",
+            "✅" if cfg['origin'] else "❌",
+            "✅" if cfg['xaxis'] else "❌",
+            "✅" if cfg['yaxis'] else "❌",
+            str(cfg['xcon']),
+            str(cfg['ycon']),
+            str(cfg['step'])
+        ]
+    )
 )
 c.print('Ctrl + D를 눌러서 나가기', style="bold red")
 
+
 def draw(func):
-	global hg
-	hg._move(hg.xMax, func(int(hg.xMax)))
-	for x in track(hg.xList):
-		try:
-			y = func(x)
-		except ZeroDivisionError as e:
-			x = hg.xList[hg.xList.index(x) + 1]
-			y = func(x)
-			hg._move(x, y)
-		else:
-			hg.turtle.goto(x, y)
-		yield x
+    global hg
+    hg.function = func
+    hg._drawFunc(track)
 
 
 hg = HG(cfg['size']['width'], cfg['size']['height'], None, cfg['step'])
 if cfg['origin']:
-	hg.originMark()
+    hg.originMark()
 if cfg['xaxis']:
-	hg.drawX()
+    hg.drawX()
 if cfg['yaxis']:
-	hg.drawY()
+    hg.drawY()
 hg.contourX(cfg['xcon'])
 hg.contourY(cfg['ycon'])
 
 while True:
-	cere = input(">: f(x)=").lower()
-	if cere == '':
-		exit()
-	try:
-		func = eval("lambda x: " + cere, util.env)
-		if type(func(1)) == int:
-			for _ in draw(func): pass
-		elif type(func(1)) == float:
-			for _ in draw(func): pass
-		else:
-			raise TypeError("함수의 값은 유리수로 정해져야합니다.")
-	except SyntaxError as e:
-		util.errorPrint(c, '수식 문법 오류가 있습니다.', e)
-	except NameError as e:
-		util.errorPrint(c, '정의되지 않은 변수입니다.', e)
-	except TypeError as e:
-		util.errorPrint(c, '수식 타입 오류가 있습니다.', e)
-	except ZeroDivisionError as e:
-		util.errorPrint(c, '분모는 0이 될 수 없습니다.', e)
-	except OverflowError as e:
-		util.errorPrint(c, '입력된 숫자가 너무 큽니다.', e)
-	except Exception as e:
-		util.errorPrint(c, '알 수 없는 오류가 발생했습니다.', e)
+    cere = input(">: f(x)=").lower()
+    if cere == '':
+        exit()
+    try:
+        func = eval("lambda x: " + cere, util.env)
+        ranX = random.random()
+        if type(func(ranX)) == int:
+            for _ in draw(func):
+                pass
+        elif type(func(ranX)) == float:
+            for _ in draw(func):
+                pass
+        else:
+            raise TypeError("함수의 값은 유리수로 정해져야합니다.")
+    except SyntaxError as e:
+        util.errorPrint(c, '수식 문법 오류가 있습니다.', e)
+    except NameError as e:
+        util.errorPrint(c, '정의되지 않은 변수입니다.', e)
+    except TypeError as e:
+        util.errorPrint(c, '수식 타입 오류가 있습니다.', e)
+    except ZeroDivisionError as e:
+        util.errorPrint(c, '분모는 0이 될 수 없습니다.', e)
+    except OverflowError as e:
+        util.errorPrint(c, '입력된 숫자가 너무 큽니다.', e)
+    except Exception as e:
+        util.errorPrint(c, '알 수 없는 오류가 발생했습니다.', e)
