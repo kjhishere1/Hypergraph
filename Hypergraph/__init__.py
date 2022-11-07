98971261f462639f3293a5bf2271cd06ffbcfcc5
kjhishere1 <kjhishere@proton.me>
2022-11-07 14:52:17 +0900

    파멸적 재구축

---
+++ Hypergraph/__init__.py
@@ -0,0 +1,9 @@
+from .hypergraph import Hypergraph
+
+from .error import HGException as Exception
+from .error import HGFunctionTypeError as FunctionTypeError
+from .error import HGImaginaryNumberError as ImaginaryNumberError
+from .error import HGOverflowError as OverflowError
+from .error import HGSyntaxError as SyntaxError
+from .error import HGUndefinedError as UndefinedError
+from .error import HGZeroDivisionError as ZeroDivisionError
