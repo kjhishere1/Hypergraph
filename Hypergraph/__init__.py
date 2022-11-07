403d0e7f638fdf2d778cfba6a765d7f659c1a039
kjhishere1 <kjhishere@proton.me>
2022-11-07 16:40:38 +0900

    WOW

--- Hypergraph/__init__.py
+++ Hypergraph/__init__.py
@@ -1,5 +1,3 @@
-from .hypergraph import Hypergraph
-
 from .error import HGException as Exception
 from .error import HGFunctionTypeError as FunctionTypeError
 from .error import HGImaginaryNumberError as ImaginaryNumberError
@@ -7,3 +5,7 @@ from .error import HGOverflowError as OverflowError
 from .error import HGSyntaxError as SyntaxError
 from .error import HGUndefinedError as UndefinedError
 from .error import HGZeroDivisionError as ZeroDivisionError
+
+from .hypergraph import Hypergraph
+Range = Hypergraph._BetterRange
+Parse = Hypergraph.Parse
--- Hypergraph/error.py
+++ Hypergraph/error.py
@@ -1,41 +1,41 @@
 class HGFunctionTypeError(Exception):
-    def __init__(self, msg: str):
+    def __init__(self, msg: str = None):
         if msg == None:
             msg = '함수의 타입이 잘못되었습니다.'
         super().__init__(msg)
 
 class HGSyntaxError(Exception):
-    def __init__(self, msg: str):
+    def __init__(self, msg: str = None):
         if msg == None:
             msg = '함수에서 문법 오류가 발생했습니다.'
         super().__init__(msg)
 
 class HGImaginaryNumberError(Exception):
-    def __init__(self, msg: str):
+    def __init__(self, msg: str = None):
         if msg == None:
             msg = '함수의 값이 허수가 될 수 없습니다.'
         super().__init__(msg) 
 
 class HGUndefinedError(Exception):
-    def __init__(self, msg: str):
+    def __init__(self, msg: str = None):
         if msg == None:
             msg = '정의되지 않은 변수가 호출되었습니다.'
         super().__init__(msg)
 
 class HGZeroDivisionError(Exception):
-    def __init__(self, msg: str):
+    def __init__(self, msg: str = None):
         if msg == None:
             msg = '분모가 0이 될 수 없습니다.'
         super().__init__(msg)
 
 class HGOverflowError(Exception):
-    def __init__(self, msg: str):
+    def __init__(self, msg: str = None):
         if msg == None:
             msg = '숫자 값이 너무 큽니다.'
         super().__init__(msg) 
 
 class HGException(Exception):
-    def __init__(self, msg: str):
+    def __init__(self, msg: str = None):
         if msg == None:
             msg = '알 수 없는 오류가 발생했습니다.'
         super().__init__(msg)
